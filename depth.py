f78f676e074396eaa308e00ae31977d4f1725933
matthew-zak <matthew.zak@tooploox.com>
2017-03-27 19:58:10 -0400

    initial commit for computing the depth

---
+++ depth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+import scipy.sparse.linalg
+
+
+
+if __name__ == '__main__':
+    normals_matrix_2 = np.load('normals.npy')
+    albedo_matrix_2 = np.load('albedos.npy')
